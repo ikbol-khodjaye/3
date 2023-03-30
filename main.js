ea85b205a15fa17c8d4607cd01b23e07c8fdd52e
ikbol-khodjaye <ikbol.khodjayev2011.77@gmail.com>
2023-03-30 17:59:17 +0500

    usha

---
+++ main.js
@@ -0,0 +1,20 @@
+let son_1 = prompt("son")-0
+let son_2 = prompt("son")-0
+function hisobla(qiymat1, qiymat2){
+ let natija = 0
+ if(!qiymat1 || !qiymat2){
+    document.body.textContent = "Qiymatlarni kirit"
+    return "Qiymatlarni kirit"
+ }else{
+
+    
+    natija = qiymat1*qiymat2
+  
+    document.body.textContent = natija
+    for(let i = 1; i<= natija; i++)
+    return natija
+ }
+}
+
+let result= hisobla(son_1, son_2)
+console.log(natija)
